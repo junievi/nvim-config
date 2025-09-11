4a351a299e92c0077a5b0c8a9d1313dd42f8784e
Eric GT <bookjunkie93@gmail.com>
2025-09-11 17:39:34 -0400

    fixing markdown-preview

--- lua/egt/plugins/markdown.lua
+++ lua/egt/plugins/markdown.lua
@@ -3,6 +3,7 @@ return {
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = "markdown",
+		keys = { { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", desc = "Toggle [M]arkdown [P]review" } },
 		build = ":call mkdp#util#install()",
 	},
 }
